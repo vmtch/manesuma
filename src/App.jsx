12a2186ecbf1f610f0e66b8ea5e98856bc75c935
pine <ktakahiro2001@gmail.com>
2024-09-27 14:19:11 +0900

    休憩時間の設定を集中モード以外に限る

--- src/App.jsx
+++ src/App.jsx
@@ -149,6 +149,7 @@ function App() {
           type="number"
           onChange={handleBreakTimeChange}
           placeholder="休憩時間を入力"
+          disabled = {isConcentrate}
         />
       </div>
       {isVisible && (
